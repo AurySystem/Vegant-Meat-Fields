a15fdfd7d9ed44140cb70217a889679b6d7f6980
Aury <22009843+AurySystem@users.noreply.github.com>
2022-08-18 14:42:59 -0400

    Setup repo

--- src/main/java/gay/aurum/ethicalmeatfields/EthicalMeatFields.java
+++ src/main/java/gay/aurum/ethicalmeatfields/EthicalMeatFields.java
@@ -9,7 +9,8 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Vegan't Meat Fields");
+	public static final String MODID = "ethicalmeatfields";
 
 	@Override
 	public void onInitialize(ModContainer mod) {
